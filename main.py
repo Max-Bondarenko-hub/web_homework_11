7a9b6f859256aa005d069e74df4ed9fa002a268f
Max Bondarenko <max.zonk@gmail.com>
2024-03-16 11:43:33 +0000

    mod main

--- main.py
+++ main.py
@@ -23,26 +23,23 @@ app.include_router(auth.router, prefix="/api")
 app.include_router(users.router, prefix="/api")
 app.include_router(profile.router, prefix='/api')
 
-
-
-
-@app.on_event("startup")
-async def startup():
-    """
-    Run tasks when the application starts.
-
-    Connects to the Redis server and initializes FastAPILimiter.
-
-    :return: None
-    """
+async def initialize_limiter():
     r = await redis.Redis(
         host=settings.redis_host, 
         port=settings.redis_port, 
         db=0, 
         encoding="utf-8", 
         decode_responses=True
-        )
+    )
     await FastAPILimiter.init(r)
+
+@app.on_event("startup")
+async def startup_event():
+    await initialize_limiter()
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await FastAPILimiter.redis.close()
 
 @app.get("/")
 def read_root():
